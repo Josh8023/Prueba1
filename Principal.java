3f6be884f33b42c71480ec4e81a296a82f333928
Joseph Sierra <alexsong8023@gmail.com>
2020-04-20 20:46:49 -0500

    nuevo xd

--- Principal.java
+++ Principal.java
@@ -9,15 +9,17 @@ public class Principal
      ArrayList <Empleado> contratistas = new ArrayList <Empleado> ();
      
      Empleado empleado1 = new Empleado ();
-     empleado1.setNombre("Joseph");
-     empleado1.setApellido("Sierra ");
+     empleado1.setNombre("Margarito");
+     empleado1.setApellido("Pilacuente");
      
      Empleado empleado2 = new Empleado ();
-     empleado1.setNombre("Joseph");
-     empleado1.setApellido("Sierra ");
+     empleado2.setNombre("Joseph");
+     empleado2.setApellido("Sierra ");
      
      contratistas.add(empleado1); //Posición 0 Array
      contratistas.add(empleado2); //Pocición 1 Array
+     
+     contratistas.forEach((emp) -> System.out.println(emp.getNombre() + " " + emp.getApellido () ));
     }
 
 }
