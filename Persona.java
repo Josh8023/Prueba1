6cfde910ecb2a7ad52b691d96267566751b4d3bc
Joseph Sierra <alexsong8023@gmail.com>
2020-04-20 21:51:16 -0500

    Nuevas Secciones

--- Persona.java
+++ Persona.java
@@ -2,7 +2,7 @@
 public class Persona
 {
 
-    String nombre, apellido, telefono, genero;
+    String nombre, apellido, telefono, genero, direccion, movil, nacimiento, ldn/*Lugar de nacimiento */;
     
     
     public Persona()
@@ -11,9 +11,15 @@ public class Persona
        
     }
     
-    public Persona (String nom, String ape) {
+    public Persona (String nom, String ape, String tel, String dir, String mov, String nac, String lugar) {
     this.nombre = nom;
     this.apellido = ape;
+    this.telefono = tel;
+    this.direccion = dir;
+    this.movil= mov;
+    this.nacimiento = nac;
+    this.ldn = lugar;
+    
     }
     
     public void setNombre(String nom){
@@ -31,4 +37,53 @@ public class Persona
     public String getApellido () {
     return this.apellido;
     }
+    
+    
+
+    public void setTelefono(String tel){
+    this.telefono = tel;
+    }
+    
+    public String getTelefono () {
+    return this.telefono;
+    }
+    
+    
+    public void setDireccion(String dir){
+    this.direccion = dir;
+    }
+    
+    public String getDireccion ( ) {
+    return this.direccion;
+    }
+    
+    
+    public void setMovil(String mov){
+    this.movil = mov;
+    }
+    
+    public String getMovil ( ) {
+    return this.movil;
+    }    
+    
+    
+    
+    public void setNacimiento(String nac){
+    this.nacimiento = nac;
+    }
+    
+    public String getNacimiento ( ) {
+    return this.nacimiento;
+    }
+    
+    
+    
+    public void setLdn(String lugar){
+    this.ldn = lugar;
+    }
+    
+    public String getLdn ( ) {
+    return this.ldn;
+    }    
+    
 }
--- Principal.java
+++ Principal.java
@@ -11,15 +11,28 @@ public class Principal
      Empleado empleado1 = new Empleado ();
      empleado1.setNombre("Margarito");
      empleado1.setApellido("Pilacuente");
+     empleado1.setTelefono("3545681");
+     empleado1.setDireccion("Calle 13 - 45 B El morro");
+     empleado1.setMovil ("3459972301");
+     empleado1.setNacimiento ("03/05/1981");
+     empleado1.setLdn ("Polo nuevo magdalena");
      
      Empleado empleado2 = new Empleado ();
      empleado2.setNombre("Joseph");
      empleado2.setApellido("Sierra ");
+     empleado2.setTelefono("2453451");
+     empleado2.setDireccion("Carrera 21 - 16 C Repelon");
+     empleado2.setMovil ("3135123456");
+     empleado2.setNacimiento ("04/06/1991"); 
+     empleado2.setLdn ("San José");
+     
      
      contratistas.add(empleado1); //Posición 0 Array
      contratistas.add(empleado2); //Pocición 1 Array
      
-     contratistas.forEach((emp) -> System.out.println(emp.getNombre() + " " + emp.getApellido () ));
+     contratistas.forEach((emp) -> System.out.println(emp.getNombre() + "\n" + emp.getApellido () + 
+     "\n" + emp.getTelefono() + "\n" + emp.getDireccion() + "\n" + emp.getMovil () + "\n" + emp.getNacimiento () + 
+     "\n" + emp.getLdn () + "\n")); 
     }
 
 }
