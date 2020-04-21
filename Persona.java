f5bdabd8e54dd83ab3c725e057961590a7142304
Joseph Sierra <alexsong8023@gmail.com>
2020-04-20 19:15:45 -0500

    Intercambio inicial de proyecto

---
+++ Persona.java
@@ -0,0 +1,19 @@
+
+public class Persona
+{
+
+    String nombre, apellido, direccion, lugar_nacimiento;
+    int numero_C, telefono, fecha_nacimiento; 
+    
+    
+    public Persona()
+    {
+     
+       
+    }
+    
+    public Persona (String nom, String ape) {
+    this.nombre = nom;
+    this.apellido = ape;
+    }
+}
